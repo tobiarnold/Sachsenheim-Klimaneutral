efd239a23e7df079883c9c0a7bedf619843b2153
tobiarnold <77453801+tobiarnold@users.noreply.github.com>
2021-12-19 17:14:29 +0100

    Update main.py

--- main.py
+++ main.py
@@ -54,7 +54,7 @@ def main():
     st.set_page_config(page_title="Wetterdaten des Deutschen Wetterdienstes", page_icon=":green_heart:", layout="wide")
     st.title("Wetterdaten des Deutschen Wetterdienstes")
     st.markdown("***")
-    image = Image.open("https://raw.githubusercontent.com/tobiarnold/Sachsenheim-Klimaneutral/main/Sachsenheim-S6.jpg")
+    image = Image.open(r"https://raw.githubusercontent.com/tobiarnold/Sachsenheim-Klimaneutral/main/Sachsenheim-S6.jpg")
     st.image(image, width=200)
     st.write("""Hallo👋 Wir sind **sachsenheim.klimaneutral**!
              Eine überparteiliche Initiative, die erreichen möchte, dass Sachsenheim bis 2035 klimaneutral wird. 
