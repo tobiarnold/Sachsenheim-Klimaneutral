d3d3d70feac58c7c4b78a65018baa69772e5412c
tobiarnold <77453801+tobiarnold@users.noreply.github.com>
2022-01-25 21:26:34 +0100

    Update main.py

--- main.py
+++ main.py
@@ -101,7 +101,7 @@ def main():
     fig = px.scatter(df_selection, x="Jahr", y="abs. Max. Lufttemperatur", trendline="ols",
                            color="abs. Max. Lufttemperatur", color_continuous_scale=px.colors.sequential.Hot_r, title="<b>maximal gemessene Lufttemperatur je Jahr in Grad Celsius</b>")
     fig.update_traces(marker_size=8)
-    fig.update_layout(coloraxis_colorbar_x=0.95, coloraxis_colorbar=dict(title="Temperatur"),margin=dict(l=0, r=0, t=50))
+    fig.update_layout(coloraxis_colorbar_x=0.95, coloraxis_colorbar=dict(title="Temperatur"),margin=dict(l=0, r=60, t=50))
     fig.update_xaxes(fixedrange=True)
     fig.update_yaxes(fixedrange=True)
     st.plotly_chart(fig, use_container_width=False)
